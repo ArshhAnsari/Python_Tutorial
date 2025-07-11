138c6fc6ace52cc9f8be92530f10601f1558ffa2
ArshhAnsari <arshhansari27@gmai.com>
2025-07-11 12:00:45 +0530

    Changes in app.py file

--- MiniProjects/Snake_Water_Gun/app.py
+++ MiniProjects/Snake_Water_Gun/app.py
@@ -1,3 +1,20 @@
+# Here's a Simple Flask App Skeleton Code that we use for the Snake-Water-Gun Game
+'''
+from flask import Flask
+
+# Create an instance of the Flask class
+app = Flask(__name__)
+
+# Define a route for the root URL
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+# Run the application
+if __name__ == '__main__':
+    app.run(debug=True)
+
+'''
 from flask import Flask, render_template, request
 import random
 
